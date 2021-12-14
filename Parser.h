a601d44fd20a05ae08b474dc176c8256fc6dfcc9
Quod6 <quod66@gmail.com>
2021-12-15 00:16:54 +0300

    Adding parser class

---
+++ Parser.h
@@ -0,0 +1,19 @@
+#include "Libs.h"
+
+class Parser
+{
+private:
+	int WINDOW_WIDTH;
+	int WINDOW_HEIGHT;
+	float SPEED;
+	float MAX_SPEED;
+	int PLAYER_COLOR;
+	int MAP_INDEX;
+	int PLAYER_COUNT;
+	int BOT_COUNT;
+
+public:
+	Parser();
+
+	void parse(string filename);
+};
